aea2079563161402aec6f8c2f9d669cc723b1dc8
gilbertnixonrombe <gilbertnixonrombe@gmail.com>
2026-03-02 18:58:02 +0700

    Fix formatting of comment in serial_sum.py

---
+++ serial_sum.py
@@ -0,0 +1,10 @@
+#Program Sequential - Serial Computing
+n = 5
+total = 0
+
+print("Serial Computation")
+for i in range(1, n + 1):
+    total += i
+    print(f"step {i}: total = {total} ")
+
+print("Final Serial sum is", total)
